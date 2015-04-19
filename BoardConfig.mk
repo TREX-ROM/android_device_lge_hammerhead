09ae6cbbae4b0d008bb593a1bd9b688fd193e692
LiquidDark <fisch890@gmail.com>
2015-04-19 09:41:18 +0200

    Hammerhead: More LIQUIFY

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -111,6 +111,14 @@ BOARD_SEPOLICY_DIRS += \
 # Define kernel config for inline building
 TARGET_KERNEL_CONFIG := slim_hammerhead_defconfig
 TARGET_KERNEL_SOURCE := kernel/lge/hammerhead
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.9/bin
+KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
+
+# LIQUIFY OPTIMIZATIONS V1
+# LIQUIFY := true
+
+# Changelog
+LIQUID_CHANGELOG := true
 
 # The list below is order dependent
 BOARD_SEPOLICY_UNION += \
