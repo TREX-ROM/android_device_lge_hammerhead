650aefe96102fda2411b69b227b699449bfad5b2
t-rex600 <fisch890@gmail.com>
2015-05-07 17:24:15 +0200

    Add new Kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -109,7 +109,7 @@ BOARD_SEPOLICY_DIRS += \
        device/lge/hammerhead/sepolicy
 
 # Define kernel config for inline building
-TARGET_KERNEL_CONFIG := slim_hammerhead_defconfig
+TARGET_KERNEL_CONFIG := liquiddark_hammerhead_defconfig 
 TARGET_KERNEL_SOURCE := kernel/lge/hammerhead
 KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.9/bin
 KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
