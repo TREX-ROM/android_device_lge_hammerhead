f50b6b4b905ac64294caad8a6847beedf82c2b94
LiquidDark <fisch890@gmail.com>
2015-11-15 13:42:03 +0100

    change slim kernel 2/2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,7 +31,7 @@ BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.selinux=permissive
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02900000 --tags_offset 0x02700000
 
 # Define kernel config for inline building
-TARGET_KERNEL_CONFIG := cyanogenmod_hammerhead_defconfig
+TARGET_KERNEL_CONFIG := hammerhead_defconfig
 TARGET_KERNEL_SOURCE := kernel/lge/hammerhead
 BOARD_KERNEL_IMAGE_NAME := zImage-dtb
 
