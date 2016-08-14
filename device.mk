f9228105b21673be9d4fb6649ea09f1edb90f31d
t-rex600 <fisch890@gmail.com>
2016-08-14 17:33:21 +0200

    Hammerhead: Use Snap camera

--- device.mk
+++ device.mk
@@ -166,7 +166,8 @@ PRODUCT_PACKAGES += \
     libmmjpeg_interface \
     camera.hammerhead \
     mm-jpeg-interface-test \
-    mm-qcamera-app
+    mm-qcamera-app \
+    Snap
 
 PRODUCT_PACKAGES += \
     keystore.msm8974
