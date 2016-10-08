4ca52fe2f55e430629d78dfd5b4b58821bb07ab8
t-rex600 <fisch890@gmail.com>
2016-10-08 15:11:30 +0200

    LDify

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -78,15 +78,8 @@ VSYNC_EVENT_PHASE_OFFSET_NS := 7500000
 SF_VSYNC_EVENT_PHASE_OFFSET_NS := 5000000
 TARGET_USES_ION := true
 
-# Enable dex-preoptimization to speed up first boot sequence
-ifeq ($(HOST_OS),linux)
-  ifeq ($(TARGET_BUILD_VARIANT),user)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-    endif
-  endif
-endif
-DONT_DEXPREOPT_PREBUILTS := true
+# Disable dex-preoptimization
+WITH_DEXPREOPT := false
 
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
